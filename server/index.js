d57f1af336460287bc8e43963bddcd9d720f9e7f
shadoeee <balaganesh541@gmail.com>
2024-04-30 22:28:47 +0530

    check

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb+srv://balaganesh541:123123123@cluster0.gkrmpai.mongodb.net/chat?retryWrites=true&w=majority&ssl=true';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
